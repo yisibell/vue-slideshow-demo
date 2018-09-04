8b2715e5bf4f51a2a5e027df09082730ad77c59c
elenh <2194516801@qq.com>
2018-09-04 11:52:30 +0800

    complete v1

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
-    <slideshow/>
+    <slideshow @jump="go"/>
   </div>
 </template>
 
 <script>
 export default {
-  
+  data(){
+    return {
+      imgList : [
+        {title: "1" , src: "./images/1.jpg"},
+        {title: "2" , src: "./images/2.jpg"},
+        {title: "3" , src: "./images/3.jpg"},
+        {title: "4" , src: "./images/4.jpg"}
+      ]
+    }
+  },
+  methods: {
+    go(data){
+      console.log(data)
+    }
+  }
 }
 </script>
--- src/slideshow/slideshow.vue
+++ src/slideshow/slideshow.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    slideshow init 
+  <div class="carousel" :style="{width: w , height: h}">
+    <ul @mouseover="stop" @mouseout="loop">
+      <li v-for="(v,i) in data" :key="i" :style="{display: curr == i ? 'block' : 'none'}">
+        <a href="javascript:;" @click="go(v)"><img :src="v.src" alt="图跑了" :title="v.title"></a>
+      </li>
+    </ul>
+  
+    <!--小圆点 S-->
+    <ol :style="{ marginLeft : '-' + dots *12 / 2 + 'px' }">
+      <li v-for="(v,i) in dots" :key="i" :class="{ on : curr == i }" @click="clickDots(i)"></li>
+    </ol>
+    
+    <!--左右箭头 S-->
+    <a href="javascript:;" class="prev" @click="prev">&lt;</a>
+    <a href="javascript:;" class="next" @click="next">&gt;</a>
+    
   </div>
 </template>
 
@@ -8,12 +22,159 @@
 export default {
   name: "slideshow",
   data(){
-    return {}
+    return {
+      curr: 0,
+      timer: null
+    }
+  },
+  props: {
+    w: {
+      type: String,
+      default: '100%'
+    },
+    h: {
+      type: String,
+      default: '340px'
+    },
+    data: {
+      type: Array,
+      default: function(){
+        return new Array({src: '../images/1.jpg' , title: "default"} , {src: '../images/2.jpg' , title: "default2"})
+      }
+    }
+    
+  },
+  computed: {
+    dots : function(){
+      return this.data.length;
+    }
+  },
+  created(){
+    this.data.forEach( (v,i) => {
+      v.id = i;
+    });
+
+    // 循环 切换
+    this.loop()
+  },
+  methods : {
+    next(){
+      this.curr != this.data.length-1 ? this.curr++ : this.curr = 0;
+    },
+    prev(){
+      this.curr != 0 ? this.curr-- : this.curr = this.data.length-1
+    },
+    clickDots(i){
+      this.curr = i;
+    },
+    go(data){
+      this.$emit('jump' , data)
+    },
+    loop(){
+
+      this.timer = setInterval( ()=>{
+        this.next()
+      } , 2000 )
+    },
+    stop(){
+      clearInterval( this.timer );
+      this.timer = null;
+    }
   }
 }
 </script>
 
 
 <style lang="scss" scoped>
+
+  ul,ol,li{
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+  a{
+    text-decoration:none;
+    display: block;
+    img{
+      display: block;
+      margin: 0 auto;
+    }
+  }
+
+  .carousel {
+    margin: 0px auto;
+    background-color: rgba(0,0,0,.5);
+    overflow: hidden;
+    position: relative;
+
+    ul{
+      width: 100%;
+      height: 100%;
+      li{
+        width: 100%;
+        height: 100%;
+        display: none;
+      }
+    }
+
+    //小圆点
+    ol{
+      position: absolute;
+      left: 50%;
+      bottom: 20px;
+      border-radius: 10px;
+      li{
+        width: 12px;
+        height: 12px;
+        background-color: #fff;
+        float: left;
+        margin: 2px 5px;
+        border-radius: 50%;
+        cursor: pointer;
+
+        &.on{
+          background-color: rgba(0,0,0,.4);
+        }
+      }
+    }
+
+    //左右箭头
+    a{
+
+      &.prev , &.next{
+        position : absolute;
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        background-color: rgba(255,255,255,0.5);
+        top : 50%;
+        margin-top:-25px;
+        line-height :50px;
+        text-align :center;
+        font-size : 24px;
+        font-family: "simsun";
+        color:#fff;
+        transition : all 1s;
+      }
+
+    }
+
+    .next{
+      right: -1000px;
+    }
+    .prev{
+      left : -1000px;
+    }
+
+    &:hover{
+      .next{ right: 10px;}
+    }
+
+    &:hover{
+      .prev{ left: 10px;}
+    }
+
+  }
+
 
 </style>
